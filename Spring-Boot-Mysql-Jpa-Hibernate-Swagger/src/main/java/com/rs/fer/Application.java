2995550e48abcd46acac01614f38a763bea6e0c5
manis <manis@192.168.1.210>
2020-09-11 20:08:43 +0530

    changes in application.java

--- Spring-Boot-Mysql-Jpa-Hibernate-Swagger/src/main/java/com/rs/fer/Application.java
+++ Spring-Boot-Mysql-Jpa-Hibernate-Swagger/src/main/java/com/rs/fer/Application.java
@@ -3,13 +3,16 @@ package com.rs.fer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 //it will handle manythings @Resource,@ComponentScan,@EnableAutoConfiguration,@Configuration
 @SpringBootApplication
 @EnableTransactionManagement
-public class Application {
+public class Application 
+{
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
 
 }
